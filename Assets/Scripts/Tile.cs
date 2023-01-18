b86e78bbe89837cccc39ee2bf8e4783926780df6
SongJH2 <117159941+SongJH2@users.noreply.github.com>
2023-01-18 21:05:22 +0900

    ~144 Clear

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] Tower towerPrefab;
+
+    [SerializeField] bool isPlaceable;
+    public bool IsPlaceable { get { return isPlaceable; }    }
+
+    GridManager gridManager;
+    Pathfinder pathfinder;
+    Vector2Int coordinates = new Vector2Int();
+
+    private void Awake()
+    {
+        gridManager = FindObjectOfType<GridManager>();
+        pathfinder = FindObjectOfType<Pathfinder>();
+    }
+
+    private void Start()
+    {
+        if(gridManager!=null)
+        {
+            coordinates = gridManager.GetCoordinatesFromPosition(transform.position);
+        }
+
+        if(!IsPlaceable)
+        {
+            gridManager.BlockNode(coordinates);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        if(gridManager.GetNode(coordinates).isWalkable && !pathfinder.WillBlockPath(coordinates))
+        {
+            bool isSueecssful = towerPrefab.CreatTower(towerPrefab, transform.position);
+            if (isSueecssful)
+            {
+                gridManager.BlockNode(coordinates);
+                pathfinder.NotifyReceivers();
+            }
+        }        
+    }
+}

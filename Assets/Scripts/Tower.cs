b197db86554aee9cfc33b5ce639c0a97304e18c7
SongJH2 <117159941+SongJH2@users.noreply.github.com>
2023-01-19 12:38:56 +0900

    ~148 Clear

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     [SerializeField] int cost = 75;
- 
+    [SerializeField] float buildDelay = 0.1f;
+
+    private void Start()
+    {
+        StartCoroutine(Build());
+    }
+
     public bool CreatTower(Tower tower, Vector3 position)
     {
         Bank bank = FindObjectOfType<Bank>();
@@ -23,5 +29,28 @@ public class Tower : MonoBehaviour
         }
 
         return false;
+    }
+
+    IEnumerator Build()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+            foreach (Transform grandChild in child)
+            {
+                grandChild.gameObject.SetActive(false);
+            }
+        }
+
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(buildDelay);
+            foreach (Transform grandChild in child)
+            {
+                grandChild.gameObject.SetActive(true);
+            }
+            
+        }
     }
 }
